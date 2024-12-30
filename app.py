638c0e4ff83f50ae5d05ab18863a7bcdb93952bb
AX99 <rapha.mensah@gmail.com>
2024-12-30 10:29:12 +0000

    app factory

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-from flask import Flask
-from firebase_admin import firestore
-from src.firebase_init.firebase_init import initialize_firebase
-from src.routes.tag_routes import tag_routes
+from factory import create_app
 
-# Initialize Flask app
-app = Flask(__name__)
-
-# Initialize Firebase
-db = initialize_firebase()
-
-# Register Blueprint
-app.register_blueprint(tag_routes, url_prefix="/api")
-
-# Example usage
-@app.route("/")
-def home():
-    return "Welcome to the NFC Contact System!"
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
