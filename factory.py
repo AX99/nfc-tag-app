f6a6a970b05a84140ae2596c696e0b3572e71459
Raphael Ako-Mensah <rapha.mensah@gmail.com>
2025-06-02 08:55:13 +0100

    factory

--- factory.py
+++ factory.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from firebase_admin import firestore
 from src.firebase_init.firebase_init import initialize_firebase
 from src.routes.tag_routes import tag_routes
 from src.routes.finder_routes import finder_routes  # Import finder_routes
